d33de05dee2646dcc66bd819734bddcfdce3ff22
kalebmiranda <97697576+kalebmiranda@users.noreply.github.com>
2023-03-30 14:58:15 -1000

    commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-  //JS
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    // REVEAL ON SCROLL JS
+    // CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
+    // CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER 
+    const myobserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.setAttribute("data-sectionstate", "active");
+        } else {
+            entry.target.setAttribute("data-sectionstate", "inactive");
+        }
+        });
+    });
+    
+    
+    document.querySelectorAll('.scroll-triggered').forEach((el) => {
+        myobserver.observe(el);
+    }); 
+
+
 });
